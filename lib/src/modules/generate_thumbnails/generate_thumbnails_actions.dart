e0b3bb21b4877b62ac3c75b1e33ed9bf931d1683
Varun Kumar <varun.kumar@indwealth.in>
2023-07-29 15:58:39 +0530

    Integrated API

--- lib/src/modules/generate_thumbnails/generate_thumbnails_actions.dart
+++ lib/src/modules/generate_thumbnails/generate_thumbnails_actions.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 import 'package:bannerbot_youtube_thumbnail/src/network.dart';
 
 Future<Map<String, dynamic>> getThumbnailsData(String prompt) {
-  String endpoint = '';
+  String endpoint =
+      'https://kt6q22xmv3zz6qxfnbjyfel5fe0lyezq.lambda-url.ap-south-1.on.aws/';
 
   return Network.postRequest(
       endpoint,
